70f8fde1c3926a3573be36a8f287f45c5c0244ee
TotzkePaul <TotzkePaul@users.noreply.github.com>
2026-09-11 03:16:46 +0000

    deploy: 1daf9ab44ce5421a1e99a1fd681bc9861cb209df

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
-const CACHE = "chess-tutor-bb3609057f6bff53";
-const ASSETS = ["./asset-manifest.json","./favicon.ico","./index.html","./logo192.png","./logo512.png","./manifest.json","./robots.txt","./static/css/main.96b40cbd.css","./static/js/123.d79d2eb7.chunk.js","./static/js/314.c1f16077.chunk.js","./static/js/314.c1f16077.chunk.js.LICENSE.txt","./static/js/453.c3edd9d2.chunk.js","./static/js/main.e729c038.js","./static/js/main.e729c038.js.LICENSE.txt"];
+const CACHE = "chess-tutor-ac74bc4a93a57700";
+const ASSETS = ["./asset-manifest.json","./favicon.ico","./index.html","./logo192.png","./logo512.png","./manifest.json","./robots.txt","./static/css/main.96b40cbd.css","./static/js/123.2cbcc772.chunk.js","./static/js/314.c1f16077.chunk.js","./static/js/314.c1f16077.chunk.js.LICENSE.txt","./static/js/453.c3edd9d2.chunk.js","./static/js/main.456c916d.js","./static/js/main.456c916d.js.LICENSE.txt"];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
